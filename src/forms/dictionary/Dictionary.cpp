04c9e52bc67fb84ac3aae2784102a9d4d0812c86
milan medlik <milan.medlik@wildfuse.com>
2011-11-10 22:22:55 +0100

    add simple elements to list

--- src/forms/dictionary/Dictionary.cpp
+++ src/forms/dictionary/Dictionary.cpp
@@ -17,3 +17,63 @@ Dictionary::~Dictionary()
 {
 	// TODO Auto-generated destructor stub
 }
+
+result Dictionary::OnInitializing(void)
+{
+	__pList = static_cast<ListView *>(GetControl(L"IDC_LESSON"));
+	__pList->SetItemProvider(*this);
+	return E_SUCCESS;
+}
+
+// ListViewItemEvents
+void OnListViewContextItemStateChanged(Osp::Ui::Controls::ListView &listView, int index, int elementId, Osp::Ui::Controls::ListContextItemStatus status)
+{
+}
+
+void OnListViewItemLongPressed(Osp::Ui::Controls::ListView &listView, int index, int elementId, bool &invokeListViewItemCallback)
+{
+}
+
+void OnListViewItemStateChanged(Osp::Ui::Controls::ListView &listView, int index, int elementId, Osp::Ui::Controls::ListItemStatus status)
+{
+}
+
+void OnListViewItemSwept(Osp::Ui::Controls::ListView &listView, int index, Osp::Ui::Controls::SweepDirection direction)
+{
+}
+
+// list item provider
+Osp::Ui::Controls::ListItemBase * Dictionary::CreateItem(int index, int itemWidth)
+{
+	ListAnnexStyle style = LIST_ANNEX_STYLE_NORMAL;
+	    CustomItem* pItem = new CustomItem();
+	    pItem->Construct(Osp::Graphics::Dimension(itemWidth,100), style);
+
+	    switch (index % 3)
+	    {
+	    case 0:
+	        pItem->AddElement(Rectangle(80, 25, 200, 50), ID_FORMAT_STRING, L"HOME", true);
+	        break;
+	    case 1:
+	        pItem->AddElement(Rectangle(80, 25, 200, 50), ID_FORMAT_STRING, L"Msg", true);
+	        break;
+	    case 2:
+	        pItem->AddElement(Rectangle(80, 25, 200, 50), ID_FORMAT_STRING, L"Alarm", true);
+	        break;
+	    default:
+	        break;
+	    }
+
+
+	    return pItem;
+}
+
+bool Dictionary::DeleteItem(int index, Osp::Ui::Controls::ListItemBase *pItem, int itemWidth)
+{
+	return false;
+}
+
+int Dictionary::GetItemCount(void)
+{
+	return 10;
+}
--- src/forms/dictionary/Dictionary.h
+++ src/forms/dictionary/Dictionary.h
@@ -9,14 +9,40 @@
 #define DICTIONARY_H_
 
 #include "../BaseWordForm.h"
+#include <FGraphics.h>
 
-class Dictionary: public BaseWordForm
+using namespace Osp::Ui;
+using namespace Osp::Ui::Controls;
+using namespace Osp::Graphics;
+using namespace Osp::Base;
+
+class Dictionary: public BaseWordForm, public Osp::Ui::Controls::IListViewItemEventListener, public Osp::Ui::Controls::IListViewItemProvider
 {
+	ListView * __pList;
+
+public:
+	static const int ID_FORMAT_STRING = 1;
+
+
 public:
 	Dictionary();
 	virtual ~Dictionary();
 
 	String  GetResourceID(){return L"IDF_DICTIONARY";}
+
+	virtual result 	OnInitializing (void);
+
+	// ListViewItemEvents
+	virtual void  OnListViewContextItemStateChanged (Osp::Ui::Controls::ListView &listView, int index, int elementId, Osp::Ui::Controls::ListContextItemStatus status);
+	virtual void  OnListViewItemLongPressed (Osp::Ui::Controls::ListView &listView, int index, int elementId, bool &invokeListViewItemCallback);
+	virtual void  OnListViewItemStateChanged (Osp::Ui::Controls::ListView &listView, int index, int elementId, Osp::Ui::Controls::ListItemStatus status);
+	virtual void  OnListViewItemSwept (Osp::Ui::Controls::ListView &listView, int index, Osp::Ui::Controls::SweepDirection direction);
+
+	// list item provider
+	virtual Osp::Ui::Controls::ListItemBase *  CreateItem (int index, int itemWidth);
+	virtual bool  DeleteItem (int index, Osp::Ui::Controls::ListItemBase *pItem, int itemWidth);
+	virtual int  GetItemCount (void);
+
 };
 
 #endif /* DICTIONARY_H_ */
