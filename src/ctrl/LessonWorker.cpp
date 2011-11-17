1bd063908809c4cdc5db0fb2d23a5ee13e8808d5
milan medlik <milan.medlik@wildfuse.com>
2011-11-17 08:40:49 +0100

    Lesson worker move to separate modeul LessonWorker.cpp

---
+++ src/ctrl/LessonWorker.cpp
@@ -0,0 +1,192 @@
+/*
+ * LessonWorker.cpp
+ *
+ *  Created on: 17.11.2011
+ *      Author: m1uan
+ */
+
+#include "LessonWorker.h"
+
+LessonWorker::LessonWorker()
+{
+	__mutex.Create();
+	__mutexWait.Create();
+	__list.Construct(10);
+	__runing = false;
+	__currentLesson = 0;
+	__stop = false;
+	__lwLissener = null;
+	// must be set true
+	// because firt in Run is first call
+	// to IsWait
+	__wait = true;
+}
+
+LessonWorker::~LessonWorker()
+{
+
+}
+
+bool LessonWorker::GetLessonFromList(int & lesson)
+{
+	__mutex.Acquire();
+
+	// reset values to stop
+	lesson = 0;
+	__currentLesson = 0;
+
+	// is something for do in list?
+	__runing = __list.GetCount() > 0;
+	if (__runing)
+	{
+		Integer *pInt = static_cast<Integer*> (__list.GetAt(0));
+		lesson = pInt->ToInt();
+		__list.RemoveAt(0, true);
+		__currentLesson = lesson;
+	}
+	__mutex.Release();
+
+	return lesson != 0;
+}
+
+bool LessonWorker::AddLesson(int lesson, bool remove)
+{
+	AppAssert(lesson != 0);
+	if (lesson == 0)
+		return false;
+
+	bool result = true;
+
+	// negative lesson is remove lesson with the id
+	lesson = !remove ? lesson : lesson * -1;
+
+	__mutex.Acquire();
+
+	int inversion = lesson * (-1);
+
+	// try add lesson which actualy in work process
+	if (lesson == __currentLesson)
+	{
+		result = false;
+		goto RELEASE;
+	}
+
+	// try add lesson which is inversion of lesson already in work process
+	if (inversion == __currentLesson)
+	{
+		// hope that isn't in list other inversion
+		// because will be catched here
+		// on previosly attempt
+		result = __list.InsertAt(*(new Integer(lesson)), 0) == E_SUCCESS;
+		__stop = true;
+
+		goto RELEASE;
+	}
+
+	// get inversion in list
+	// when somebody check lesson and after that uncheck
+
+	for (int i = 0; i != __list.GetCount(); i++)
+	{
+		Integer *pInt = static_cast<Integer*> (__list.GetAt(0));
+		if (inversion == pInt->ToInt())
+		{
+			__list.RemoveAt(i, true);
+			result = true;
+			goto RELEASE;
+		}
+	}
+
+	// get exist in list
+	for (int i = 0; i != __list.GetCount(); i++)
+	{
+		Integer *pInt = static_cast<Integer*> (__list.GetAt(0));
+		if (pInt->ToInt() == lesson)
+		{
+			result = false;
+			break;
+		}
+	}
+
+	// add to list if not exist in list
+	if (result)
+		result = __list.Add(*(new Integer(lesson))) == E_SUCCESS;
+	RELEASE: __mutex.Release();
+
+	return result;
+}
+
+bool LessonWorker::IsRunning()
+{
+	bool isRunning;
+	__mutex.Acquire();
+	isRunning = __runing;
+	__mutex.Release();
+	return isRunning;
+}
+
+Object *LessonWorker::Run(void)
+{
+	int lesson = 0;
+
+	while (IsWait())
+	{
+		while (GetLessonFromList(lesson))
+		{
+			// TODO: add lessons
+
+			__mutex.Acquire();
+			if (__lwLissener)
+				__lwLissener->OnLessonTask(lesson);
+			__mutex.Release();
+		}
+	}
+	return null;
+}
+
+void LessonWorker::SetLessonWorkerLissener(ILessonWorkerLissener *lesson)
+{
+	__mutex.Acquire();
+	__lwLissener = lesson;
+	__mutex.Release();
+}
+
+bool LessonWorker::IsWait()
+{
+	bool wait;
+	__mutexWait.Acquire();
+	wait = __wait;
+
+	// wait is can set only one time
+	// because other will be the cycle in Run
+	// will not over
+	if(__wait)
+		__wait = false;
+
+	__mutexWait.Release();
+	return wait;
+}
+
+bool LessonWorker::StartWait()
+{
+	__mutexWait.Acquire();
+	__wait = true;
+	return true;
+}
+
+void LessonWorker::StopWait()
+{
+	__mutex.Acquire();
+
+	// the running status is will be changed
+	// on GetLessonFromList when if still true
+	// it means the thread is not checking items in __list
+	// ( it is middle on process adding/deleting some lesson )
+	// and claimly can be wait removed
+	if (__runing && __wait)
+		__wait = false;
+
+	// release from StartWait
+	__mutexWait.Release();
+	__mutex.Release();
+}
---
+++ src/ctrl/LessonWorker.h
@@ -0,0 +1,92 @@
+/*
+ * LessonWorker.h
+ *
+ *  Created on: 17.11.2011
+ *      Author: m1uan
+ */
+#ifndef LESSONWORKER_H_
+#define LESSONWORKER_H_
+
+#include <FIo.h>
+#include <FBase.h>
+
+using namespace Osp::Io;
+using namespace Osp::Base;
+using namespace Osp::Base::Runtime;
+using namespace Osp::Base::Collection;
+
+class ILessonWorkerLissener
+{
+public:
+	ILessonWorkerLissener()
+	{
+	}
+	;
+
+	virtual ~ILessonWorkerLissener()
+	{
+	}
+	;
+public:
+
+	virtual void OnLessonTask(const int lesson) = 0;
+};
+
+class LessonWorker: public Osp::Base::Runtime::Thread
+{
+	/**
+	 * mutex
+	 */
+	Mutex __mutex;
+	Mutex __mutexWait;
+	/**
+	 * is now working or not
+	 */
+	bool __runing;
+
+	/**
+	 * can continu in Run process
+	 * because user can stop the adding proces uncheck the lesson
+	 */
+	bool __stop;
+
+	/**
+	 * list with other works, lesson waiting to do (add, remove)
+	 */
+	ArrayList __list;
+
+	/*
+	 * lesson already worket on
+	 */
+	int __currentLesson;
+
+	/**
+	 * i can't explain it (magic variable)
+	 */
+	bool __wait;
+	/**
+	 * after every lesson done (add, remove) is called
+	 * ILessonWorkerLissener::OnLessonDone
+	 */
+	ILessonWorkerLissener * __lwLissener;
+
+public:
+	LessonWorker();
+	~LessonWorker();
+
+public:
+
+	bool GetLessonFromList(int & lesson);
+	bool AddLesson(int lesson, bool remove);
+
+	bool IsRunning();
+	Object *Run(void);
+
+	void SetLessonWorkerLissener(ILessonWorkerLissener *lesson);
+
+	bool IsWait();
+	bool StartWait();
+	void StopWait();
+};
+
+#endif /* LESSONWORKER_H_ */
